1649ca5f5f2f4f5b16ea7139c3868c0c7224f040
Prabin Dhungana <154338768+godDPrabin@users.noreply.github.com>
2026-05-12 23:42:24 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     base: '/peak-experience-web/',
-    // This tells the builder to create a static index.html
     prerender: {
       routes: ['/'],
     },
